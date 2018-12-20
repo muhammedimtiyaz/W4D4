809567b04fcf8ee20a87ae772bc28a061174fcff
App Academy Student <muhammedimtiyaz87@gmail.com>
2018-12-20 11:57:32 -0500

    finish users and sessions views and add heler methods

--- MusicApp/app/controllers/sessions_controller.rb
+++ MusicApp/app/controllers/sessions_controller.rb
@@ -1,11 +1,25 @@
 class SessionsController < ApplicationController
-  def create
 
-    
+  def new
+    @user = User.new
+    render :new    
+  end
+  
+  def create
+    email = params[:user][:email]
+    password = params[:user][:password]
+    @user = User.find_by_credentials(email, password)
+    if @user
+      session[:session_token] = @user.reset_session_token!
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ['Invalid password']
+      render :new
+    end    
   end
 
   def destroy
-    
-    
+    logout!
+    redirect_to new_session_url
   end
 end
